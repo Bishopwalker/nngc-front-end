717990d31d62fc8926b0ecba1395fdfb439eb4ab
Bishop Walker <bishop.walker@sbd2.com>
2023-09-14 12:59:37 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,11 @@ import Signup from "./components/signup-dk/SignUp";
 import Dashboard from "./components/dashboard/Dashboard";
 // @ts-ignore
 import ClassAppointments from "./components/appointment/ClassAppointments.jsx";
+import DKAppointment from "./components/appointment-dk/DKAppointment";
 import Dumpster from "./components/products/Dumpster";
-import NotFound from "./components/notFound/NotFound";
-import VerifyEmail from "./components/verifyEmail/VerifyEmail";
-import DumpsterDK from "./components/products/DumpsterDK";
 import TrashSubscription from "./components/products/TrashSubscription";
 import G_Maps from "./components/google/G_Maps";
 import Encoded_GMaps from "./components/google/EncodedG_Maps";
-
 import NavBottom from "./components/navBottom/NavBottom";
 // @ts-ignore
 import Blog from "./components/blog/Blog";
@@ -27,46 +24,42 @@ import OurStory from "./components/ourStory/OurStory.jsx";
 import { PersistGate } from 'redux-persist/integration/react';
 import SuccessPage from "./components/checkout/SuccessPage";
 
-
 function App() {
-const screenTitle = useAppSelector(state => state.title)
+	const screenTitle = useAppSelector(state => state.title)
 
 	React.useEffect(() => {
 		document.title = screenTitle.title? screenTitle.title : 'NNGC'
 	}, [screenTitle])
-  return (
-    <div className="App">
+	return (
+		<div className="App">
 
-		<HeaderTop />
-		<Navbar />
-		<NavBottom />
+			<HeaderTop />
+			<Navbar />
+			<NavBottom />
 
 			<Routes>
-			  <Route path="/" element={<Home />} />
+				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />
 				<Route path="/signup" element={<Signup />} />
-				<Route path="/appointment" element={<ClassAppointments/>} />
+				<Route path="/appointment" element={<DKAppointment/>} />
 				<Route path="/dashboard" element={<Dashboard />} />
-				<Route path="/dumpster/:productId" element={<DumpsterDK/>} />
+				<Route path="/dumpster/:productId" element={<Dumpster/>} />
 				<Route path="/res_trash_sub" element={<TrashSubscription/>} />
 				<Route path='/maps' element={<G_Maps/>} />
 				<Route path='/emaps' element={<Encoded_GMaps/>} />
-
-				<Route path="*" element={<NotFound />} />
-				<Route path="/verify" element={<VerifyEmail />} />
-
 				<Route path='/blog' element={<Blog/>} />
 				<Route path='services' element={<Service/>} />
 				<Route path='story' element={<OurStory/>} />
 				<Route path='/success' element={<SuccessPage/>} />
 				<Route path="*" element={<h1>Not Found</h1>} />
 
-
 			</Routes>
 
-		<Footer />
-    </div>
-  )
+			<Footer />
+		</div>
+	)
 }
 
 export default App
+
+
