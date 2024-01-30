53955c358b1640ab817e7ee26e75bfe7d836e3f8
Bishop Walker <walkerbishop333@gmail.com>
2024-01-30 11:44:36 -0500

    Google Reviews

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -26,50 +26,55 @@ const GoogleReviewsTitle = () => {
         </div>
     );
 };
+
+const hardcodedReviews = [
+    {
+        author_name: "T Dickie",
+        relative_time_description: "2 weeks ago",
+        rating: 5,
+        text: "I highly commend Northern Neck Garbage Collection for their exceptional service. Their prompt and efficient waste management solutions have consistently exceeded my expectations...",
+    },
+    {
+        author_name: "Israel The Voice",
+        relative_time_description: "2 months ago",
+        rating: 5,
+        text: "I recently signed up for their service, and I have been extremely satisfied and happy with the convenience and easy process of them, removing my trash from my home...",
+
+    },
+    {
+        author_name: "Leslye McDade-Morrison",
+        relative_time_description: "a month ago",
+        rating: 5,
+        text: "We cleaned out our garage and had lots to haul away. Mr. Walker did a great job, he loaded up his trailer and the bed of his truck...",
+
+    },
+    {
+        author_name: "Rodney Ream",
+        relative_time_description: "a week ago",
+        rating: 5,
+        text: "The crew is very nice and funny, I would highly recommend these guys.",
+
+    },
+    {
+        author_name: "Victoria Johnson-Walker",
+        relative_time_description: "a week ago",
+        rating: 5,
+        text: "Every Thursday he picks up my trash!!"
+
+    }
+];
+
+
+
 const Reviews = () => {
-    const [reviews, setReviews] = useState([]);
-    const [error, setError] = useState(false); // New state for tracking errors
-
-    const fetchReviews = async () => {
-        try {
-            const response = await axios('https://api.northernneckgarbage.com/nngc/googleReviews/getReviews');
-            const data = response.data.result.reviews;
-            setReviews(data);
-        } catch (err) {
-            console.error(err);
-            setError(true); // Set error state to true if an error occurs
-        }
-    };
-
-    useEffect(() => {
-      //  fetchReviews().then((c)=>console.log(c));
-    }, []);
-
     const renderStars = (rating) => {
         return [...Array(rating)].map((_, index) => <span key={index}>⭐</span>);
     };
 
-    if (error) {
-        return(
-        <Box minHeight={'100px'}>
-            <GoogleReviewsTitle/>
-            <br/>
-        <h4>
-            ....error showing Google Reviews at this time</h4>
-        </Box>
-        )
-    }
-
     return (
         <Box minHeight={'200px'}>
-            <Helmet>
-                <title>Customer Reviews for Northern Neck Garbage Collection</title>
-                <meta name="keywords" content="Northern Neck Garbage Collection reviews, customer feedback, waste management services, garbage collection, junk removal, recycling services, environmental cleanliness, customer satisfaction" />
-                <meta name="description" content="Read the latest Google reviews from our satisfied customers. Learn why Northern Neck Garbage Collection is a trusted name in waste management services." />
-                {/* You can add more meta tags as needed */}
-            </Helmet>
-            <GoogleReviewsTitle/>
-            {reviews.map((review, index) => (
+            <GoogleReviewsTitle />
+            {hardcodedReviews.map((review, index) => (
                 <Card key={index} sx={{ marginBottom: 2 }}>
                     <h1>{renderStars(review.rating)}</h1>
                     <CardHeader
@@ -88,6 +93,3 @@ const Reviews = () => {
 };
 
 export default Reviews;
-
-
-
