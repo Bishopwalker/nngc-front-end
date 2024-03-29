a09c3a812b5e2254ca853dd8f44c9365dfe78992
Bishop Walker <Bishop.Walker@SBD2.com>
2024-03-29 06:50:18 -0400

    working on customizing the map

--- src/components/google/EncodedG_Maps.tsx
+++ src/components/google/EncodedG_Maps.tsx
@@ -87,6 +87,7 @@ const Encoded_GMaps: React.FC = () => {
                 //   console.log(data)
                });
        }
+
     }, [isLoaded,routeNumber,trigger]);
 
     const handlePolylineClick = (index: number) => {
@@ -125,7 +126,7 @@ const Encoded_GMaps: React.FC = () => {
 
     };
 
-//console.log("selected customer",instructions)
+console.log("selected customer",instructions)
 
     return (
         <ThemeProvider theme={theme}>
@@ -138,126 +139,133 @@ const Encoded_GMaps: React.FC = () => {
                     <Grid item container sm={12} spacing={2} alignItems="stretch">
                         <Grid item xs={12} sm={12} md={10} lg={2}>
 
-                    <Box style={{ border: '1px solid black', marginBottom: '1em', width:'300px',overflow:'auto' }}>
-             <Typography variant="h6" align="left" sx={{
-                fontWeight: 'bold',
-                color: '#2C3E50',
-                letterSpacing: '1px',
-                textTransform: 'uppercase',
-                }}>
-             Total Stops: {totalStops}
-             </Typography>
+                            <Box style={{
+                                border: '1px solid black',
+                                marginBottom: '1em',
+                                width: '300px',
+                                overflow: 'auto'
+                            }}>
+                                <Typography variant="h6" align="left" sx={{
+                                    fontWeight: 'bold',
+                                    color: '#2C3E50',
+                                    letterSpacing: '1px',
+                                    textTransform: 'uppercase',
+                                }}>
+                                    Total Stops: {totalStops}
+                                </Typography>
 
-                <Typography variant="h6" align="left" sx={{
-                fontWeight: 'bold',
-                color: '#2C3E50',
-                letterSpacing: '1px',
-                textTransform: 'uppercase',
-                }}>
-Total Miles: {totalMiles/10}
-                </Typography>
-                <Typography variant="h6" align="left" sx={{
-                fontWeight: 'bold',
-                color: '#2C3E50',
-                letterSpacing: '1px',
-                textTransform: 'uppercase',
-                }}>
-Total Min: {totalTime/10}
-                </Typography>
+                                <Typography variant="h6" align="left" sx={{
+                                    fontWeight: 'bold',
+                                    color: '#2C3E50',
+                                    letterSpacing: '1px',
+                                    textTransform: 'uppercase',
+                                }}>
+                                    Total Miles: {totalMiles / 10}
+                                </Typography>
+                                <Typography variant="h6" align="left" sx={{
+                                    fontWeight: 'bold',
+                                    color: '#2C3E50',
+                                    letterSpacing: '1px',
+                                    textTransform: 'uppercase',
+                                }}>
+                                    Total Min: {totalTime / 10}
+                                </Typography>
 
-                <h3>Customer List</h3>
-                <button onClick={handleSortByName}>Sort by Name</button>
-                <button onClick={handleSortById}>Sort by ID</button>
-                <ul>
-                    {customerList.map((customer, index) => (
-                        <li
-                            key={customer.customerInfo.id}
-                            style={selectedCustomer === index ? { backgroundColor: 'yellow' } : {}}
-                            onClick={() => handleCustomerClick(index)}
-                        >
-                            {customer.customerInfo.fullName} (ID: {customer.customerInfo.id})
+                                <h3>Customer List</h3>
+                                <button onClick={handleSortByName}>Sort by Name</button>
+                                <button onClick={handleSortById}>Sort by ID</button>
+                                <ul>
+                                    {customerList.map((customer, index) => (
+                                        <li
+                                            key={customer.customerInfo.id}
+                                            style={selectedCustomer === index ? {backgroundColor: 'yellow'} : {}}
+                                            onClick={() => handleCustomerClick(index)}
+                                        >
+                                            {customer.customerInfo.fullName} (ID: {customer.customerInfo.id})
 
-                        </li>
-                    ))}
-                </ul>
-            </Box>
-            <div id="customer-info">
-                {selectedCustomerInfo && (
-                    <div>
-                        <h3>{selectedCustomerInfo.fullName} (ID: {selectedCustomerInfo.id})</h3>
-                        <p>Phone Number: {selectedCustomerInfo.phoneNumber}</p>
-                      <ul>
-                          {/* @ts-ignore */}
-                        <li>Address:
-                            {selectedCustomerInfo.address}
-                        </li>
-                      </ul>
-                        {/* ...other information */}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </Box>
+
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={4}>
+                            <div style={{
+                                border: '1px solid black',
+                                width: '100%',
+                                height: 'auto'
+                            }}> {/* Adjust height as needed */}
+
+                                <GoogleMap
+
+                                    mapContainerStyle={mapContainerStyle}
+                                    zoom={10}
+                                    center={selectedLat && selectedLon ? {lat: selectedLat, lng: selectedLon} : path[0]}
+                                    /*@ts-ignore*/
+                                    selectedCustomerInfo={selectedCustomerInfo}
+
+                                >
+                                    <Polyline
+                                        path={path}
+                                        options={{strokeColor: '#FF0000', strokeOpacity: 1.0, strokeWeight: 2}}
+                                        onClick={(e) => {
+                                            if (e.latLng) {  // Check if e.latLng is not null
+                                                const index = Math.floor(e.latLng.lat());
+                                                handlePolylineClick(index);
+                                            } else {
+                                                console.log("latLng is null");
+                                            }
+                                        }}
+                                    />
+                                    {path.length > 0 && (
+                                        <Marker
+                                            position={path[path.length - 1]}  // Position of the last point in the path array
+                                        />
+                                    )}
+                                    {selectedLat && selectedLon && (
+                                        <Marker
+                                            position={{lat: selectedLat, lng: selectedLon}}
+                                            icon={{
+                                                url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png', // You can use a custom icon here
+                                                scaledSize: new window.google.maps.Size(40, 40),
+                                            }}
+                                        />
+                                    )}
+                                </GoogleMap>
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <div id="customer-info">
+                        {selectedCustomerInfo && (
+                            <div>
+                                <h3>{selectedCustomerInfo.fullName} (ID: {selectedCustomerInfo.id})</h3>
+                                <p>Phone Number: {selectedCustomerInfo.phoneNumber}</p>
+                                <ul>
+                                    {/* @ts-ignore */}
+                                    <li>Address:
+                                        {selectedCustomerInfo.address}
+                                    </li>
+                                </ul>
+                                {/* ...other information */}
+                            </div>
+                        )}
                     </div>
-                )}
-            </div>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4}>
-                        <div style={{ border: '1px solid black', width: '100%', height: 'auto' }}> {/* Adjust height as needed */}
-
-                            <GoogleMap
-
-                mapContainerStyle={mapContainerStyle}
-                zoom={10}
-                center={selectedLat && selectedLon ? { lat: selectedLat, lng: selectedLon } : path[0]}
-                /*@ts-ignore*/
-                             selectedCustomerInfo={selectedCustomerInfo}
-
-            >
-                <Polyline
-                    path={path}
-                    options={{ strokeColor: '#FF0000', strokeOpacity: 1.0, strokeWeight: 2 }}
-                    onClick={(e) => {
-                        if (e.latLng) {  // Check if e.latLng is not null
-                            const index = Math.floor(e.latLng.lat());
-                            handlePolylineClick(index);
-                        } else {
-                            console.log("latLng is null");
-                        }
-                    }}
-                />
-                {path.length > 0 && (
-                    <Marker
-                        position={path[path.length - 1]}  // Position of the last point in the path array
-                    />
-                )}
-                {selectedLat && selectedLon && (
-                    <Marker
-                        position={{ lat: selectedLat, lng: selectedLon }}
-                        icon={{
-                            url: 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png', // You can use a custom icon here
-                            scaledSize: new window.google.maps.Size(40, 40),
-                        }}
-                    />
-                )}
-            </GoogleMap>
-                        </div>
-                    </Grid>
-                    </Grid>
-
-                    <Grid item xs={12} style={{ width: '100%' }}>
-                        <Typography
-                           >
-                            {!selectedCustomerInfo && <><Box mt={4} pt={4} display={{xs: 'block'}}>
-
-                            </Box><Box mt={4} pt={4} display={{xs: 'block'}}>
-
-                            </Box><Box mt={4} pt={4} display={{xs: 'block'}}>
-
-                            </Box></>
-                         }
-            </Typography>
+                    <Grid item xs={12} style={{width: '100%'}}>
+                        <Typography component='div'>
+                            {!selectedCustomerInfo && <>
+                                <Box mt={4} pt={4} display={{xs: 'block'}}>
+                                    <h3>Click on a customer to view their instructions</h3>
+                                </Box>
+                            </>}
+                        </Typography>
                     </Grid>
                 </Grid>
-                </Box>
-            <Grid item xs={12} style={{ width: '100%' }}>
-                <Typography component='div' style={{ width: '100%',
-                    border: '1px solid black', padding: '1em' }}>
+            </Box>
+            <Grid item xs={12} style={{width: '100%'}}>
+                <Typography component='div' style={{
+                    width: '100%',
+                    border: '1px solid black', padding: '1em'
+                }}>
                     {selectedInstruction && (
                         <div>
                             <h3>ID: {selectedCustomerInfo?.id} {selectedCustomerInfo?.fullName}
@@ -267,11 +275,13 @@ Total Min: {totalTime/10}
                             <span dangerouslySetInnerHTML={{ __html: selectedInstruction }} />
                         </div>
                     )}
-                    {instructions.map((instruction: { instruction: any; }, index: React.Key | null | undefined) => (
+                    {instructions.map((instruction: {
+                        customerInfo: CustomerInfo;
+                        instruction: any; }, index: React.Key | null | undefined) => (
                       <div key={index}>
 
                           <h3>Step { (index as number)  + 1}</h3>
-
+                          {selectedCustomerInfo &&  <h5>{selectedCustomerInfo.fullName} {selectedCustomerInfo.id}</h5>}
                         <span  dangerouslySetInnerHTML={{ __html: instruction.instruction }}  />
                        </div>
                     ))}
